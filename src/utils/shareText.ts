9b6f9ef4392951036d1494b4a4de03f3b8bb7afc
Samuel Franklin <samuelfranklinff@gmail.com>
2026-09-20 16:38:23 -0300

    Update shareText.ts

--- src/utils/shareText.ts
+++ src/utils/shareText.ts
@@ -112,7 +112,7 @@ export function formatShareText(data: ShareBillData): string {
     });
 
     lines.push(separator);
-    lines.push('_Gerado com Party Billy Divisor_');
+    lines.push('_Gerado com https://party-billy-divisor.vercel.app/_');
 
     return lines.join('\n');
 }
